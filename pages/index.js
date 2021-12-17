e625871284d088695738c7b026b14a6c19aa22b4
Dinesh Tamang <dineshtamang7263@gmail.com>
2021-12-17 13:38:45 +0530

    added authentication login & logout

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Login from '../components/Login'
 import { useMoralis } from "react-moralis";
 
 export default function Home() {
-  const { isAuthenticated } = useMoralis();
+  const { isAuthenticated, logout } = useMoralis();
     
   if(!isAuthenticated) return <Login />
     
@@ -15,6 +15,7 @@ export default function Home() {
       </Head>
 
       <h1>Welcome to the App</h1>
+      <button className="bg-yellow-500 p-5 ml-3 rounded-lg mt-2 font-bold" onClick={logout}>Logout</button>
     </div>
   )
 }
